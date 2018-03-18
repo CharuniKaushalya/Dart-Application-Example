ca41121cd4d0a178869da966496077ebd2b3d33f
CharuniKaushalya <snkaushi@gmail.com>
2018-03-18 19:49:09 +0530

    if else..

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@ import 'dart:html';
 
 void main() {
   querySelector('#heading').text = 'Simple Dart Web Application';
-  querySelector('#output').text = 'Some text.....';
+  querySelector('#output').text = 'Please use left, right, up and down..........';
 
   //add click event get value of the text box
   querySelector('#submit').onClick.listen(clickSubmitButton);
@@ -12,5 +12,18 @@ void main() {
 clickSubmitButton(e) {
   print("Yes");
   InputElement input = querySelector('#commands');
-  querySelector('#output').appendHtml("<div>${input.value}</div>");
+  String output = "";
+  if(input.value == "left") {
+    output = "You are going left";
+  }else if(input.value == "right") {
+    output = "You are going right";
+  }else if(input.value == "up") {
+    output = "You are going up";
+  }else if(input.value == "down") {
+    output = "You are going down";
+  }else {
+    output = "Please use left, right, up and down..........";
+  }
+  var outputHtml = "<div>${output }</div>";
+  querySelector('#output').appendHtml(outputHtml);
 }
