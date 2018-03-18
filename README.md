85b7975c93ca1cea6ffc714cb8930a1d9a3c1934
CharuniKaushalya <snkaushi@gmail.com>
2018-03-18 18:07:51 +0530

    styles

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ An absolute bare-bones web app.
 
 Created from templates made available by Stagehand under a BSD-style
 [license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
+
+
+TO get all dependencies,
+  run pub get
+
+To run web application,
+  pub  serve web --port 8080
--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,16 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  querySelector('#heading').text = 'Simple Dart Web Application';
+  querySelector('#output').text = 'Some text.....';
+
+  //add click event get value of the text box
+  querySelector('#submit').onClick.listen(clickSubmitButton);
+
+}
+
+clickSubmitButton(e) {
+  print("Yes");
+  InputElement input = querySelector('#commands');
+  querySelector('#output').appendHtml("<div>${input.value}</div>");
 }
